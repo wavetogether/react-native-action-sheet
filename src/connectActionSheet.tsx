a8e616703a626920d9239cacae221a1ab72a767b
babo <seirion@mensakorea.org>
2021-06-11 16:57:24 +0900

    add forwardRef

--- src/connectActionSheet.tsx
+++ src/connectActionSheet.tsx
@@ -3,20 +3,35 @@ import { Consumer } from './context';
 import hoistNonReactStatic from 'hoist-non-react-statics';
 import { ActionSheetProps } from './types';
 
+interface WithForwardRefProps {
+  forwardedRef: React.Ref<React.ComponentType<ActionSheetProps>>;
+}
+
 export default function connectActionSheet<OwnProps = any>(
   WrappedComponent: React.ComponentType<OwnProps & ActionSheetProps>
 ) {
-  const ConnectedActionSheet = (props: OwnProps) => {
+  const ConnectedActionSheet = (props: WithForwardRefProps & OwnProps) => {
+    const { forwardedRef } = props;
     return (
       <Consumer>
         {({ showActionSheetWithOptions }) => {
           return (
-            <WrappedComponent {...props} showActionSheetWithOptions={showActionSheetWithOptions} />
+            <WrappedComponent
+              ref={forwardedRef}
+              {...props}
+              showActionSheetWithOptions={showActionSheetWithOptions}
+            />
           );
         }}
       </Consumer>
     );
   };
 
-  return hoistNonReactStatic(ConnectedActionSheet, WrappedComponent);
+  const ForwardedComponent = React.forwardRef(
+    (props: OwnProps & ActionSheetProps, ref: React.Ref<React.ComponentType<ActionSheetProps>>) => (
+      <ConnectedActionSheet {...props} forwardedRef={ref} />
+    )
+  );
+
+  return hoistNonReactStatic(ForwardedComponent, WrappedComponent);
 }
